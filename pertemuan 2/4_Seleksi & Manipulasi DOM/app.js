87b2d1f4351a3a25d712d14a7e50956810dfa907
endyyogatcr-star <endyyogatcr@gmail.com>
2026-06-14 13:50:58 +0700

    materi 2

--- pertemuan 2/4_Seleksi & Manipulasi DOM/app.js
+++ pertemuan 2/4_Seleksi & Manipulasi DOM/app.js
@@ -4,9 +4,9 @@ const hargaLabel = document.getElementById("harga-tampil");
 const statusNotif = document.getElementById("notif-status");
 
 // 2. Mengubah isi teks (Manipulation)
-judul.innerText = "Genshin Impact";
-hargaLabel.innerText = "Rp 150.000";
+//judul.innerText = "Genshin Impact";
+//hargaLabel.innerText = "Rp 150.000";
 
 // 3. Mengubah gaya CSS melalui JS
-statusNotif.style.color = "blue";
-statusNotif.style.fontStyle = "italic";
+//statusNotif.style.color = "blue";
+//statusNotif.style.fontStyle = "italic";
